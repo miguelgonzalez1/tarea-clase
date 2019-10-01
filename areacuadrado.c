5540c065dd49164bbb332db79338b108314d86ae
miguelgonzalez1 <55765119+miguelgonzalez1@users.noreply.github.com>
2019-10-01 14:21:48 -0500

    Add files via upload

---
+++ areacuadrado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define pi 3,1416
+
+void main()
+{
+    float radio,area=0;
+    printf("Ingrese el radio");
+    scanf("%f",&radio);
+    area=pi*radio*radio;
+    printf("El area es %f, area");
+}
+
